60ef5efbe0a95e8a1b0a318a5e7722cb3fdda859
chchung <chungchaehyeon@gmail.com>
2024-06-13 00:18:55 +0900

    boj

--- 4673.py
+++ 4673.py
@@ -1,1 +1,15 @@
-print("hello world")
+def d(n):
+    return n+sum(int(i) for i in str(n))
+
+not_self_nums = set()
+for n in range(1,10000):
+    not_self_nums.add(d(n))
+
+all_nums = set()
+for i in range(10000):
+    all_nums.add(i)
+
+self_nums = list(all_nums - not_self_nums)
+self_nums.sort()
+for i in self_nums:
+    print(i)
