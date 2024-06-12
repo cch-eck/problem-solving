93ed2b72a3ab1eb5396829a9d3d4084d56c650d3
chchung <chungchaehyeon@gmail.com>
2024-06-13 00:22:08 +0900

    .

--- 4673.py
+++ 4673.py
@@ -6,7 +6,7 @@ for n in range(1,10000):
     not_self_nums.add(d(n))
 
 all_nums = set()
-for i in range(10000):
+for i in range(1,10000):
     all_nums.add(i)
 
 self_nums = list(all_nums - not_self_nums)
